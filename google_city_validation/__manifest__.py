8d39e89f8dda9f9ae684dcd42657dfc876903fd2
Yasmine El Mrini <yasmine.elmrini@savoirfairelinux.com>
2018-03-29 18:00:57 -0400

    [10.0] Add google_city_validation

---
+++ google_city_validation/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# © 2018 Savoir-faire Linux
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    'name': 'Google City Validation',
+    'version': '10.0.1.0.1',
+    'author': 'Savoir-faire Linux',
+    'maintainer': 'Savoir-faire Linux',
+    'website': 'http://www.savoirfairelinux.com',
+    'license': 'LGPL-3',
+    'category': 'Base',
+    'summary': 'City validation',
+    'depends': [
+        'base',
+        'web',
+    ],
+    'data': [
+        'views/assets.xml',
+    ],
+    'installable': True,
+}
